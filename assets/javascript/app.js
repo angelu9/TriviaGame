0b02b4dbac4cfc6735f71df9717eb0224396b788
angel m <angelmartinez@Angels-MBP.lan1>
2019-07-10 22:11:27 -0400

    last commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var trivia = [
     },
     {
         question: "What was the New England Patriots original name?",
-        answers: ["The New England Patriots", "The Boston Patriots", "The Bay State Patriots", "The Eastern Patriots,"],
+        answers: ["The New England Patriots", "The Boston Patriots", "The Bay State Patriots", "The Eastern Patriots",],
         rightAnswer: "The Boston Patriots",
     },
     {
@@ -47,6 +47,7 @@ var card = $("#game");
 
 var timer;
 
+
 var game = {
     correct: 0,
     incorrect: 0,
@@ -54,9 +55,9 @@ var game = {
 
     countdown: function () {
         game.counter--;
-        $("#counter-number").text(game.counter);
+        $("#counter-number").html(game.counter);
         if (game.counter === 0) {
-
+            console.log("TIME UP");
             game.done();
         }
     },
@@ -65,10 +66,10 @@ var game = {
         timer = setInterval(game.countdown, 1000);
 
         $("#sub-wrapper").prepend(
-            "<h2>Time Left: <span id='counter-number'>120</span> Seconds</h2>"
+            "<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>"
         );
 
-        $(".btn").remove();
+        $("#start").remove();
 
         for (var i = 0; i < trivia.length; i++) {
             card.append("<h2>" + trivia[i].question + "</h2>");
@@ -78,7 +79,39 @@ var game = {
             }
         }
 
-        
+        card.append("<button id='done'>Done</button>");
+    },
+
+    done: function () {
+        var inputs = card.children("input:checked");
+        for (var i = 0; i < inputs.length; i++) {
+            if ($(inputs[i]).val() === trivia[i].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        }
+        this.result();
+    },
+
+    result: function () {
+        clearInterval(timer);
+
+        $("#sub-wrapper h2").remove();
+
+        card.html("<h2>All Done!</h2>");
+        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    }
+};
+
+
+$(document).on("click", ".btn", function () {
+    game.start();
+});
+
+$(document).on("click", "#done", function () {
+    game.done();
 });
 
 
